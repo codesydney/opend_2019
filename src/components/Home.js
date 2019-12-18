29d662dfd27c5c80dc896c0ff8b3da650b5136fa
Interest1024 <leo.liubin@gmail.com>
2019-12-19 00:47:37 +1100

    try to set value for textfield

--- src/components/Home.js
+++ src/components/Home.js
@@ -54,17 +54,9 @@ export default function Home() {
             </Typography>
 
             <EditIcon />
-            {/* <!input type="text" id="rapidAddress" size="100" placeholder="Type in address here" className="autocomplete" />
+            <input type="text" id="rapidAddress" size="100" placeholder="Type in address here" className="autocomplete" />
 
-            <Typography variant="body1" align="right">
-              Address Type Ahead lookup API is powered by
-              <a href="https://harmonyrightaddress.com" target="_blank" rel="noopener noreferrer" >
-                <img className="responsive-img" src="HARMONYRightAddressSmall.png" alt="Harmony RightAddress" />
-              </a>
-            </Typography>
-
-            <AutoCompleteAddress />
-            <br /> */}
+            <br />
             <MyAutoComplete />
             <Typography variant="body1" align="right">
               Address Type Ahead lookup API is powered by
@@ -73,7 +65,6 @@ export default function Home() {
               </a>
             </Typography>
 
-            <AutoCompleteAddress />
 
           </Paper>
         </Grid>
--- src/components/MyAutoComplete.js
+++ src/components/MyAutoComplete.js
@@ -12,44 +12,18 @@ import fetchJsonp from "fetch-jsonp";
 const API_URL = "https://preview-hosted.mastersoftgroup.com/harmony/rest/au/address?sourceOfTruth=GNAF&transactionID=a53d240365e6b75d65f5bf70d951289f&Authorization=Basic%20YWx1c2VyOlBselhpV3hxVUd4R094NXIycFNjamUyUWllYUV4YlY4&state=";
 //const API_URL = "https://preview-hosted.mastersoftgroup.com/harmony/rest/au/address?callback=FRED&sourceOfTruth=GNAF&transactionID=a53d240365e6b75d65f5bf70d951289f&Authorization=Basic%20YWx1c2VyOlBselhpV3hxVUd4R094NXIycFNjamUyUWllYUV4YlY4&state=";
 
-function renderSuggestion(suggestionProps) {
-  const {
-    suggestion,
-    index,
-    itemProps,
-    highlightedIndex,
-    selectedItem
-  } = suggestionProps;
-  const isHighlighted = highlightedIndex === index;
-  const isSelected = (selectedItem || "").indexOf(suggestion.title) > -1;
-
-  return (
-    <MenuItem
-      {...itemProps}
-      key={index}
-      selected={isHighlighted}
-      component="div"
-      style={{
-        fontWeight: isSelected ? 500 : 400
-      }}
-    >
-      {suggestion.fullAddress}
-    </MenuItem>
-  );
-}
-
-renderSuggestion.propTypes = {
-  highlightedIndex: PropTypes.oneOfType([
-    PropTypes.oneOf([null]),
-    PropTypes.number
-  ]).isRequired,
-  index: PropTypes.number.isRequired,
-  itemProps: PropTypes.object.isRequired,
-  selectedItem: PropTypes.string.isRequired,
-  suggestion: PropTypes.shape({
-    label: PropTypes.string.isRequired
-  }).isRequired
-};
+// renderSuggestion.propTypes = {
+//   highlightedIndex: PropTypes.oneOfType([
+//     PropTypes.oneOf([null]),
+//     PropTypes.number
+//   ]).isRequired,
+//   index: PropTypes.number.isRequired,
+//   itemProps: PropTypes.object.isRequired,
+//   selectedItem: PropTypes.string.isRequired,
+//   suggestion: PropTypes.shape({
+//     label: PropTypes.string.isRequired
+//   }).isRequired
+// };
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -87,11 +61,59 @@ export default function MyAutoComplete() {
 
   const emptyList = [];
   const [data, setData] = useState(emptyList);
-  const [query, setQuery] = useState("redux");
+  const [query, setQuery] = useState("");
   const [url, setUrl] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [finalAddress, setFinalAddress] = useState("");
+
+  const handleSelectItem = (event, index) => {
+    console.log('select item, index = ', index);
+    console.log('select item, value = ', data[index]);
+    setQuery(data[index].fullAddress);
+    console.log('select item, query = ', query);
+    //console.log('GNAF PID : ' + suggestion.id);
+    // console.log('Address  : ' + suggestion.fullAddress);
+    // console.log('Subdwelling : ' + ui.item.subdwelling);
+    // console.log('Postcode    : ' + ui.item.postcode);
+    // console.log('Longitude   : ' + ui.item.attributes.Longitude);
+    // console.log('Latitude    : ' + ui.item.attributes.Latitude);
+    // document.getReportForm.postcode.value=ui.item.postcode;
+    // document.getReportForm.longitude.value=ui.item.attributes.Longitude;
+    // document.getReportForm.latitude.value=ui.item.attributes.Latitude;
+    // document.getReportForm.fullAddress.value=ui.item.fullAddress;
+    // document.getElementById("HomeMap").setAttribute("lat", ui.item.attributes.Latitude);
+    // document.getElementById("HomeMap").setAttribute("lng", ui.item.attributes.Longitude);
+  }
+  
+  const renderSuggestion = (suggestionProps) => {
+    const {
+      suggestion,
+      index,
+      itemProps,
+      highlightedIndex,
+      selectedItem
+    } = suggestionProps;
+    const isHighlighted = highlightedIndex === index;
+    const isSelected = (selectedItem || "").indexOf(suggestion.title) > -1;
+    //const suggestionAny = <Any>suggestion;
+  
+    return (
+      <MenuItem
+        {...itemProps}
+        key={index}
+        selected={isHighlighted}
+        component="div"
+        style={{
+          fontWeight: isSelected ? 500 : 400
+        }}
+        onClick = {event => handleSelectItem(event, index)}
+      >
+        {suggestion.fullAddress}
+      </MenuItem>
+    );
+  }
+  
 
   const getSuggestions = (value, { showEmpty = false } = {}) => {
     console.log("getSuggestions", data);
@@ -99,15 +121,14 @@ export default function MyAutoComplete() {
   };
 
   const handleInputChange = event => {
-
-    const query = event.target.value;
-    setQuery(query);
-    console.log("query=", event.target.value);
-    if (query.length >= 3) {
-      setUrl(`${API_URL}&fullAddress=${query}`);
+    const input= event.target.value;
+    console.log("input=", input);
+    if (input.length >= 3) {
+      setUrl(`${API_URL}&fullAddress=${input}`);
     } else {
       setData(emptyList);
     }
+    //setQuery(input);
   };
 
   const renderInput = inputProps => {
@@ -123,17 +144,18 @@ export default function MyAutoComplete() {
           },
           ...InputProps
         }}
+        value={query}
         onChange={handleInputChange}
         {...other}
       />
     );
   };
 
-  const handleClick = (event) => {
-    console.log("final query = ", query);
-    setFinalAddress(query);
-    console.log("final address = ", finalAddress);  
-  }
+  // const handleClick = (event) => {
+  //   console.log("final query = ", query);
+  //   setFinalAddress(query);
+  //   console.log("final address = ", finalAddress);  
+  // }
 
   /*
     useEffect(() => {
@@ -202,16 +224,39 @@ export default function MyAutoComplete() {
             placeholder: "Type in address here"
           });
 
+          const { InputProps, ref, ...other } = {
+                fullWidth: true,
+                label: "",
+                InputLabelProps: getLabelProps({ shrink: true }),
+                InputProps: { onBlur, onFocus },
+                inputProps
+              };
+
           return (
             <div className={classes.container}>
-              {renderInput({
+               {/* {renderInput({
                 fullWidth: true,
                 classes,
                 label: "",
                 InputLabelProps: getLabelProps({ shrink: true }),
                 InputProps: { onBlur, onFocus },
                 inputProps
-              })}
+              })}  */}
+              <TextField
+                InputProps={{
+                  inputRef: ref,
+                  classes: {
+                    root: classes.inputRoot,
+                    input: classes.inputInput
+                  },
+                  ...InputProps
+                }}
+                value={query}
+                onChange={handleInputChange}
+                {...other}
+              />
+
+              <TextField id="standard-basic" label="Standard" value={query}/>
 
               <div {...getMenuProps()}>
                 {isOpen ? (
@@ -233,7 +278,7 @@ export default function MyAutoComplete() {
           );
         }}
       </Downshift>
-      <button type="button" onClick={handleClick}>
+      <button type="button" >
         Search
       </button>
     </div>
