a737ac83c1c9d6acd2532884afbc3ceb804e4270
Interest1024 <leo.liubin@gmail.com>
2019-12-20 23:45:47 +1100

    change to use Autocomplete sample code from material ui 4.8.0

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 //import { connect } from 'react-redux'
 import { AutoCompleteAddress } from './AutoCompleteAddress';
-import MyAutoComplete from './MyAutoComplete';
+import MyAutocomplete from './MyAutocomplete';
 
 import HomeMap from './HomeMap';
 import { makeStyles } from '@material-ui/core/styles';
@@ -53,21 +53,26 @@ export default function Home() {
               </Box>
             </Typography>
 
-            <EditIcon />
-            <input type="text" id="rapidAddress" size="100" placeholder="Type in address here" className="autocomplete" />
+            <Grid container spacing={3}>
+              <Grid item xs={0} md={2}>
+              </Grid>
+              <Grid item xs={12} md={8}>
+                <MyAutocomplete />
+              </Grid>
+              <Grid item xs={0} md={2}>
+              </Grid>
+            </Grid>
+                
 
-            <br />
-            <MyAutoComplete />
             <Typography variant="body1" align="right">
               Address Type Ahead lookup API is powered by
               <a href="https://harmonyrightaddress.com" target="_blank" rel="noopener noreferrer" >
                 <img className="responsive-img" src="HARMONYRightAddressSmall.png" alt="Harmony RightAddress" />
               </a>
             </Typography>
-
-
           </Paper>
         </Grid>
+
         <Grid item xs={12} md={4}>
           <Paper className={classes.paper}>
             <HomeInfo />
@@ -78,6 +83,7 @@ export default function Home() {
             <HomeMap name="HomeMap" lat="-33.8688" lng="151.2093" zoom="16" />
           </Paper>
         </Grid>
+
       </Grid>
     </div>
   );
--- src/components/MyAutoComplete.js
+++ src/components/MyAutoComplete.js
@@ -1,286 +1,76 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-//import deburr from "lodash/deburr";
-import Downshift from "downshift";
-import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
 import TextField from "@material-ui/core/TextField";
-import Paper from "@material-ui/core/Paper";
-import MenuItem from "@material-ui/core/MenuItem";
-//import axios from "axios";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import fetchJsonp from "fetch-jsonp";
 
-const API_URL = "https://preview-hosted.mastersoftgroup.com/harmony/rest/au/address?sourceOfTruth=GNAF&transactionID=a53d240365e6b75d65f5bf70d951289f&Authorization=Basic%20YWx1c2VyOlBselhpV3hxVUd4R094NXIycFNjamUyUWllYUV4YlY4&state=";
-//const API_URL = "https://preview-hosted.mastersoftgroup.com/harmony/rest/au/address?callback=FRED&sourceOfTruth=GNAF&transactionID=a53d240365e6b75d65f5bf70d951289f&Authorization=Basic%20YWx1c2VyOlBselhpV3hxVUd4R094NXIycFNjamUyUWllYUV4YlY4&state=";
+const API_URL="https://preview-hosted.mastersoftgroup.com/harmony/rest/au/address?sourceOfTruth=GNAF&transactionID=a53d240365e6b75d65f5bf70d951289f&Authorization=Basic%20YWx1c2VyOlBselhpV3hxVUd4R094NXIycFNjamUyUWllYUV4YlY4";
+export default function MyAutocomplete() {
+  //const [open, setOpen] = React.useState(false);
+  const [options, setOptions] = React.useState([]);
+  const [query, setQuery] = React.useState("");
+  const [searchAddress, setSearchAddress] = React.useState({});
+  //  const loading = open && options.length === 0;
+  const loading = false;
 
-// renderSuggestion.propTypes = {
-//   highlightedIndex: PropTypes.oneOfType([
-//     PropTypes.oneOf([null]),
-//     PropTypes.number
-//   ]).isRequired,
-//   index: PropTypes.number.isRequired,
-//   itemProps: PropTypes.object.isRequired,
-//   selectedItem: PropTypes.string.isRequired,
-//   suggestion: PropTypes.shape({
-//     label: PropTypes.string.isRequired
-//   }).isRequired
-// };
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    height: 250
-  },
-  container: {
-    flexGrow: 1,
-    position: "relative"
-  },
-  paper: {
-    position: "absolute",
-    zIndex: 1,
-    marginTop: theme.spacing(1),
-    left: 0,
-    right: 0
-  },
-  chip: {
-    margin: theme.spacing(0.5, 0.25)
-  },
-  inputRoot: {
-    flexWrap: "wrap"
-  },
-  inputInput: {
-    width: "auto",
-    flexGrow: 1
-  },
-  divider: {
-    height: theme.spacing(2)
-  }
-}));
-
-export default function MyAutoComplete() {
-  const classes = useStyles();
-
-  const emptyList = [];
-  const [data, setData] = useState(emptyList);
-  const [query, setQuery] = useState("");
-  const [url, setUrl] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [finalAddress, setFinalAddress] = useState("");
-
-  const handleSelectItem = (event, index) => {
-    console.log('select item, index = ', index);
-    console.log('select item, value = ', data[index]);
-    setQuery(data[index].fullAddress);
-    console.log('select item, query = ', query);
-    //console.log('GNAF PID : ' + suggestion.id);
-    // console.log('Address  : ' + suggestion.fullAddress);
-    // console.log('Subdwelling : ' + ui.item.subdwelling);
-    // console.log('Postcode    : ' + ui.item.postcode);
-    // console.log('Longitude   : ' + ui.item.attributes.Longitude);
-    // console.log('Latitude    : ' + ui.item.attributes.Latitude);
-    // document.getReportForm.postcode.value=ui.item.postcode;
-    // document.getReportForm.longitude.value=ui.item.attributes.Longitude;
-    // document.getReportForm.latitude.value=ui.item.attributes.Latitude;
-    // document.getReportForm.fullAddress.value=ui.item.fullAddress;
-    // document.getElementById("HomeMap").setAttribute("lat", ui.item.attributes.Latitude);
-    // document.getElementById("HomeMap").setAttribute("lng", ui.item.attributes.Longitude);
-  }
-  
-  const renderSuggestion = (suggestionProps) => {
-    const {
-      suggestion,
-      index,
-      itemProps,
-      highlightedIndex,
-      selectedItem
-    } = suggestionProps;
-    const isHighlighted = highlightedIndex === index;
-    const isSelected = (selectedItem || "").indexOf(suggestion.title) > -1;
-    //const suggestionAny = <Any>suggestion;
-  
-    return (
-      <MenuItem
-        {...itemProps}
-        key={index}
-        selected={isHighlighted}
-        component="div"
-        style={{
-          fontWeight: isSelected ? 500 : 400
-        }}
-        onClick = {event => handleSelectItem(event, index)}
-      >
-        {suggestion.fullAddress}
-      </MenuItem>
-    );
-  }
-  
-
-  const getSuggestions = (value, { showEmpty = false } = {}) => {
-    console.log("getSuggestions", data);
-    return data;
-  };
-
-  const handleInputChange = event => {
-    const input= event.target.value;
-    console.log("input=", input);
-    if (input.length >= 3) {
-      setUrl(`${API_URL}&fullAddress=${input}`);
-    } else {
-      setData(emptyList);
-    }
-    //setQuery(input);
-  };
-
-  const renderInput = inputProps => {
-    const { InputProps, classes, ref, ...other } = inputProps;
-
-    return (
-      <TextField
-        InputProps={{
-          inputRef: ref,
-          classes: {
-            root: classes.inputRoot,
-            input: classes.inputInput
-          },
-          ...InputProps
-        }}
-        value={query}
-        onChange={handleInputChange}
-        {...other}
-      />
-    );
-  };
-
-  // const handleClick = (event) => {
-  //   console.log("final query = ", query);
-  //   setFinalAddress(query);
-  //   console.log("final address = ", finalAddress);  
-  // }
-
-  /*
-    useEffect(() => {
+  React.useEffect(() => {
+    if (query.length >= 3) {
+    const url = `${API_URL}&fullAddress=${query}`;
     console.log("url=",url);
-    if (url === "") { return; }
-    fetchJsonp(url, {
-      jsonpCallbackFunction: 'FRED',
-    })
-      .then((response) => {
-        return response.json()
-      }).then((json) => {
-        setData(json.payload)
-        console.log('parsed json', json)
-      }).catch((ex) => {
-        console.log('parsing failed', ex)
-      })
-  }, [url]);
-
-*/
-  useEffect(() => {
-    console.log("url=",url);
-    if (url === "") { return; }
     fetchJsonp(url)
       .then((response) => {
         return response.json()
       }).then((json) => {
-        setData(json.payload)
-        console.log('parsed json', json)
+        console.log('options=', json.payload);
+        setOptions(json.payload)
       }).catch((ex) => {
-        console.log('parsing failed', ex)
+        console.log('parsing failed', ex);
       })
-  }, [url]);
+    } else {
+        setOptions([]);
+    }
+  }, [query]);
 
-/*
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
-      try {
-        const result = await axios(url);
-        //console.log("result=", result.data);
-        setData(result.data);
-      } catch (error) {
-        setIsError(true);
-      }
-      setIsLoading(false);
-    };
-    fetchData();
-  }, [url]);
-*/
+  const handleInputChange = event => {
+    console.log("input=", event.target.value);
+    setQuery(event.target.value);
+  };
+
+  const handleOptionChange = (event, value) => {
+    console.log("optionchange=", event.target.value, value);
+    setSearchAddress(value);
+  };
 
   return (
-    <div className={classes.root}>
-      <Downshift id="downshift-simple">
-        {({
-          getInputProps,
-          getItemProps,
-          getLabelProps,
-          getMenuProps,
-          highlightedIndex,
-          inputValue,
-          isOpen,
-          selectedItem
-        }) => {
-          const { onBlur, onFocus, ...inputProps } = getInputProps({
-            placeholder: "Type in address here"
-          });
-
-          const { InputProps, ref, ...other } = {
-                fullWidth: true,
-                label: "",
-                InputLabelProps: getLabelProps({ shrink: true }),
-                InputProps: { onBlur, onFocus },
-                inputProps
-              };
-
-          return (
-            <div className={classes.container}>
-               {/* {renderInput({
-                fullWidth: true,
-                classes,
-                label: "",
-                InputLabelProps: getLabelProps({ shrink: true }),
-                InputProps: { onBlur, onFocus },
-                inputProps
-              })}  */}
-              <TextField
-                InputProps={{
-                  inputRef: ref,
-                  classes: {
-                    root: classes.inputRoot,
-                    input: classes.inputInput
-                  },
-                  ...InputProps
-                }}
-                value={query}
-                onChange={handleInputChange}
-                {...other}
-              />
-
-              <TextField id="standard-basic" label="Standard" value={query}/>
-
-              <div {...getMenuProps()}>
-                {isOpen ? (
-                  <Paper className={classes.paper} square>
-                    { 
-                      getSuggestions(inputValue).map((suggestion, index) =>
-                      renderSuggestion({
-                        suggestion,
-                        index,
-                        itemProps: getItemProps({ item: suggestion.fullAddress }),
-                        highlightedIndex,
-                        selectedItem 
-                      })
-                    )}
-                  </Paper>
+    <Autocomplete
+      id="asynchronous-demo"
+      //style={{ width: 1000}}
+      disableClearable
+      getOptionSelected={(option, value) => option === value}
+      getOptionLabel={option => option.fullAddress}
+      options={options}
+      loading={loading}
+      onChange={handleOptionChange}
+      renderInput={params => (
+        <TextField
+          {...params}
+          label="Type in address here"
+          fullWidth
+          variant="outlined"
+          onChange={handleInputChange}
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <React.Fragment>
+                {loading ? (
+                  <CircularProgress color="inherit" size={20} />
                 ) : null}
-              </div>
-            </div>
-          );
-        }}
-      </Downshift>
-      <button type="button" >
-        Search
-      </button>
-    </div>
+                {params.InputProps.endAdornment}
+              </React.Fragment>
+            )
+          }}
+        />
+      )}
+    />
   );
 }
